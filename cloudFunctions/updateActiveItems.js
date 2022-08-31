a2e4e0937d30d40186137dbadf217f9e814ef888
sandalwoods <1764076874@qq.com>
2022-08-31 21:40:01 +0800

    add moralis cloud function

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -11,6 +11,22 @@ Moralis.Cloud.afterSave("ItemListed", async (request) => {
         logger.info("Found Item!")
         const ActiveItem = Moralis.Object.extend("ActiveItem")
 
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("nftAddress", request.object.get("nftAddress"))
+        query.equalTo("tokenId", request.object.get("tokenId"))
+        query.equalTo("marketplaceAddress", request.object.get("address"))
+        query.equalTo("seller", request.object.get("seller"))
+        const alreadyListedItem = await query.first()
+        if (alreadyListedItem) {
+            logger.info(`Deleting already listed ${alreadyListedItem.id}`)
+            await alreadyListedItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it's already listed`
+            )
+        }
+
         const activeItem = new ActiveItem()
         activeItem.set("marketplaceAddress", request.object.get("address"))
         activeItem.set("nftAddress", request.object.get("nftAddress"))
@@ -24,5 +40,67 @@ Moralis.Cloud.afterSave("ItemListed", async (request) => {
         )
         logger.info("Saving...")
         await activeItem.save()
+    }
+})
+
+Moralis.Cloud.afterSave("ItemCanceled", async (request) => {
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info(`Marketplace | Object: ${request.object}`)
+    if (confirmed) {
+        const ActiveItem = Moralis.Object.extend("ActiveItem")
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("marketplaceAddress", request.object.get("address"))
+        query.equalTo("nftAddress", request.object.get("nftAddress"))
+        query.equalTo("tokenId", request.object.get("tokenId"))
+        logger.info(`Marketplace | Query: ${query}`)
+        const canceledItem = await query.first()
+        logger.info(`Marketplace | CanceledItem: ${JSON.stringify(canceledItem)}`)
+        if (canceledItem) {
+            logger.info(`Deleting ${canceledItem.id}`)
+            await canceledItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it was canceled. `
+            )
+        } else {
+            logger.info(
+                `No item canceled with address: ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")} found.`
+            )
+        }
+    }
+})
+
+Moralis.Cloud.afterSave("ItemBought", async (request) => {
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info(`Marketplace | Object: ${request.object}`)
+    if (confirmed) {
+        const ActiveItem = Moralis.Object.extend("ActiveItem")
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("marketplaceAddress", request.object.get("address"))
+        query.equalTo("nftAddress", request.object.get("nftAddress"))
+        query.equalTo("tokenId", request.object.get("tokenId"))
+        logger.info(`Marketplace | Query: ${query}`)
+        const boughtItem = await query.first()
+        logger.info(`Marketplace | BoughtItem: ${JSON.stringify(boughtItem)}`)
+        if (boughtItem) {
+            logger.info(`Deleting ${boughtItem.id}`)
+            await boughtItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it was bought. `
+            )
+        } else {
+            logger.info(
+                `No item with address: ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")} found.`
+            )
+        }
     }
 })
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import { useMoralisQuery } from "react-moralis"
 
 export default function Home() {
-  // How do we show the recently listed NFTs
-  // We will index the events off-chain and then ready from our database
-  // Setup a server to listen for those events to be fired, and we will add them to a database to query
-  // TheGraph does this in a decentralized way
-  // Moralis does it in a centralized way and comes with a ton of other features
-  return (
-    <div className={styles.container}>
-      hello
-    </div>
-  )
+    // How do we show the recently listed NFTs
+    // We will index the events off-chain and then ready from our database
+    // Setup a server to listen for those events to be fired, and we will add them to a database to query
+    // TheGraph does this in a decentralized way
+    // Moralis does it in a centralized way and comes with a ton of other features
+    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+        // TableName
+        // Function for the query
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    )
+    console.log(listedNfts)
+
+    return <div className={styles.container}>hello</div>
 }
- 
